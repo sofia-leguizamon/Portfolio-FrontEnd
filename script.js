a0912c8cfcbb88f7c264a39fb2680700f6279202
sofia-leguizamon <sofiavioleta.leguizamon@gmail.com>
2023-02-19 20:40:21 -0300

    boton de edicion

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 pentitle="SCSS Arrow Animation";
+
+function cambiar_parrafo(){
+    document.getElementById("edit-acercade").style.display="block";
+    let texto = document.getElementById("text-acercade").innerText;
+    console.log(texto);
+  };
+  function cambiar_texto(valor){
+    document.getElementById("text-acercade").innerText=valor;
+  };
+
+  function logMessage(message){
+      console.log(message+"<br>");
+  };
+
+  //controla si se presiono enter 
+  let textarea=document.getElementById("edit-acercade")
+  textarea.addEventListener('keyup', (e) => {
+      logMessage('Key "${e.key}" released [event: keyup]');
+      if (e.key=="Enter"){
+          document.getElementById("edit-acercade").style.display="none"
+      }
+  });
